a23904634b4573defe5a330a40428726243450fd
Linus Sherwin <99864722+linus-sherwin@users.noreply.github.com>
2023-04-09 22:52:08 -0400

    Updated changes in app.js

--- app.js
+++ app.js
@@ -1,31 +1,22 @@
 const express = require("express");
 const bodyParser = require("body-parser");
+const date = require(__dirname + "/date.js")
 
 const app = express();
-var items = ["File Tax"];
-var workItems = [];
+const items = ["Wake Up"];
+const workItems = [];
 
 app.set ('view engine' , 'ejs');
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static("public"))
 
 app.get("/", function(req, res){
-   var today = new Date();
-
-   var options = {
-    weekday: 'long',
-    day: 'numeric',
-    month: 'long'
-   }
-
-   var day = today.toLocaleDateString("en-US", options);
-
-   
+   const day = date.getDate();
    res.render("list", {listTitle: day, newListItems: items})
 });
 
 app.post("/", function(req, res){
-    let item = req.body.newItem;
+    const item = req.body.newItem;
     if (req.body.button === 'Work List') {
         workItems.push(item);
         res.redirect("/work");
